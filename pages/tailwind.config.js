5a885a4c208571605ec8fc98f28742555d4a3d69
Rafael Marcos <rafix995@gmail.com>
2024-06-05 02:24:54 +0100

    Main page responsivity

--- pages/tailwind.config.js
+++ pages/tailwind.config.js
@@ -5,9 +5,12 @@ module.exports = {
     extend: {
       colors: {
         "uefa-blue" : "#143cdb",
+        "tour-blue" : "#004f9e",
       },
       backgroundImage: {
-        "cristiano-small" : "url('./images/cristiano-small.jpg')",
+        "cristiano-small" : "url('../images/cristiano-small.jpg')",
+        "mbappe-large"    : "url('../images/mbappe-large.jpg')",
+        "vini-medium"     : "url('../images/vini-medium.jpg')",
       }
     },
   },
